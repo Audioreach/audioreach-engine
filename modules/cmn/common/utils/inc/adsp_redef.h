05de23c0e59a85e5f099827054704925ec3cf81a
Annemarie Porter <quic_annemari@quicinc.com>
2025-03-13 11:59:33 -0700

    Revert "modules: Adds cmn and audio folders"
    This reverts commit bb974ba57e8864ef5eddddcb77cc630d962cd818.

--- modules/cmn/common/utils/inc/adsp_redef.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Qualcomm Innovation Center, Inc. All Rights Reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#define log2table adsp_log2table
-#define sqRootLut adsp_sqRootLut
-#define invSqRootLut adsp_invSqRootLut
-#define invTable adsp_invTable
